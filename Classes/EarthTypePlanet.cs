fc9a0938b0c92cb164b8a99ec9b11bb5aff31084
Sobira <sobira204@gmail.com>
2020-05-18 22:15:17 +0500

    add new class of EarthTypePlanet

---
+++ Classes/EarthTypePlanet.cs
@@ -0,0 +1,29 @@
+using SOLID.BaseClasses;
+
+namespace SOLID.Classes
+{
+    public class EarthTypePlanet : IPlanet
+    {
+        public EarthTypePlanet(string name, long weight, double diameter){
+            Name = name;
+            Weight = weight;
+            Diameter = diameter;
+        }
+        public string Name     { get; set ; }
+        public long Weight     { get; set ; }
+        public double Diameter { get; set ; }
+        public void Action (IPlanetActions act)
+        {
+            act.Do(Name);
+        }
+        public bool InhabitStatus{get;set;}
+        public void GetInhabitStatus(){
+            if(InhabitStatus){
+                System.Console.WriteLine($"{Name} is inhabited");
+            }        
+            else{
+                System.Console.WriteLine($"{Name} is'n inhabited" );
+            }
+        }
+    }
+}
